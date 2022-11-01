3cca76fafa3bec75842c84cf66d049ffe3e03ad3
Andrea Pesaresi <andrea.pesaresi3@studio.unibo.it>
2022-11-01 22:47:47 +0100

    Fatto tutto io ovviamente

--- components/componentePartita.js
+++ components/componentePartita.js
@@ -7,113 +7,64 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 import BouncyCheckboxGroup from "react-native-bouncy-checkbox-group";
 import ICheckboxButton from "react-native-bouncy-checkbox-group";
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import Tabella from "./componenteTabellaSingola"
+
 var RandTools = require('./rand_tools.js');
 
 export default class CreaPartita extends React.Component {
     constructor(props) { //per passare proprieta did ef acnhe il navigation
         super(props);
         this.state = {
-
-            tableData1: this.generaCarta(),
+            tabelle: [this.generaCarta(),this.generaCarta(),this.generaCarta()],
             numero: '',
         }
-
     }
+
     generaNumero = (numero) => {
         numero = Math.floor(Math.random() * 90) + 1;
         this.setState({ numero: numero })
     }
+
     generaCarta = () => {
        
         const tools = new RandTools(); 
         const extract_pool = [];
         var card = [[], [], []];
 
-        // Inizializzo un array per colonna
         for (var i = 0; i < 9; i++) {
             extract_pool[i] = new RandTools();
             extract_pool[i].distRandInit((i * 10) + 11, (i * 10) + 1);
         }
 
-        // Estrazione per ogni riga
         for (var i = 0; i < 9; i++) {
             card[0].push(extract_pool[i].distRandNext());
             card[1].push(extract_pool[i].distRandNext());
             card[2].push(extract_pool[i].distRandNext());
         }
 
-        // Buco la prima riga
         tools.distRandInit(9);
-        for (var i = 0; i < 4; i++) card[0][tools.distRandNext()] = -1;
+        for (var i = 0; i < 4; i++) card[0][tools.distRandNext()] = "";
 
-        // BuGo la seconda riga
         tools.distRandInit(9);
-        for (var i = 0; i < 4; i++) card[1][tools.distRandNext()] = -1;
+        for (var i = 0; i < 4; i++) card[1][tools.distRandNext()] = "";
 
-        // Buco la terza riga in funzione delle righe
-        // precendenti (ogni colonna deve avere almeno un numero)
         tools.distRandInit(9);
         var buchi = 0;
         while (buchi < 4) {
             const hit = tools.distRandNext();
 
             if (card[0][hit] != -1 || card[1][hit] != -1) {
-                card[2][hit] = -1;
+                card[2][hit] = "";
                 buchi++;
             }
         }
-        
 
-        return card;
-    }
-
-
-    _alertIndex(indexRow,indexCell) {
-        //Alert.alert(`This is row ${index + 1}`);
-        let newArr = [...this.state.tableData1]; // copying the old datas array
-        // a deep copy is not needed as we are overriding the whole object below, and not setting a property of it. this does not mutate the state. // 
-        if(newArr[indexRow][indexCell].includes("X")){
-            newArr[indexRow][indexCell] = newArr[indexRow][indexCell].replace("X","")
-        }else{
-            newArr[indexRow][indexCell] = newArr[indexRow][indexCell] + "X";
+        for(var i = 0; i < card.length; i++){
+            for(var j = 0; j < card[0].length; j++){ 
+                card[i][j] = card[i][j].toString()
+            }
         }
-        this.setState({tableData1: newArr})
-    }
-
-    renderRow(row,indexRow){
-        return(
-            <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
-                {
-                    row.map((cell,indexCell) => {
-                        console.log(cell)
-                        return (
-                            this.renderCell(cell,indexRow,indexCell)
-                        )
-                    })
-                }
-            </View>
-        )
-    }
-
-    renderCell(cell,indexRow,indexCell){
-        console.log(cell)
-        return(
-            <View style={{ flex: 1, alignSelf: 'stretch', borderWidth: "2px", borderColor: "black", color: "black !important", backgroundColor: "#e28743" }}>
-                {
-                    cell != "" ? 
-                        cell.includes("X") ? 
-                            <TouchableHighlight onPress={() => {this._alertIndex(indexRow,indexCell)}}>
-                                <Text style={{color: "black !important", textAlign: "center", paddingBottom: "5%", paddingTop: "5%"}}>X</Text>
-                            </TouchableHighlight>
-                        : 
-                        <TouchableHighlight onPress={() => {this._alertIndex(indexRow,indexCell)}}>
-                            <Text style={{color: "black !important",textAlign: "center", paddingBottom: "5%", paddingTop: "5%"}}>{cell}</Text>
-                        </TouchableHighlight>
-                    :
-                    <TouchableHighlight><Text></Text></TouchableHighlight>
-                }
-            </View>
-        )
+        return card;
     }
 
     render() {
@@ -144,15 +95,16 @@ export default class CreaPartita extends React.Component {
         return (
 
             <View style={styles.containerPartita}>
-                <View style={{ height: '20%', flexDirection: 'column', alignItems: 'center' }}>
-                    {
-                        this.state.tableData1.map((row,index) => {
-                            return (
-                                this.renderRow(row,index)
-                            )
-                        })
-                    }
-                </View>
+                {
+                    this.state.tabelle.map((tabella) => {
+                        return (
+                            <>
+                                <Tabella tabella={tabella} />
+                                <View  style={{height: 25}}/>
+                            </>
+                        )
+                    })
+                }
                 <Text style={styles.titleNick}>il numero e: {this.state.numero}</Text>
                 <Button color="red" title='Genera Numero' onPress={() => this.generaNumero()}></Button>
                 <BouncyCheckboxGroup style={{ color: 'red', marginBottom: '5%', marginTop: '5%', flexDirection: 'row', justifyContent: 'center' }}
---
+++ components/componenteTabellaSingola.js
@@ -0,0 +1,77 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, Alert, TouchableHighlight } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/stack';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import BouncyCheckboxGroup from "react-native-bouncy-checkbox-group";
+import ICheckboxButton from "react-native-bouncy-checkbox-group";
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+
+export default class Tabella extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tabella: props.tabella
+        }
+    }
+
+    renderRow(row,indexRow){
+        return(
+            <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', maxHeight: 50}}>
+                {
+                    row.map((cell,indexCell) => {
+                        console.log(cell)
+                        return (
+                            this.renderCell(cell,indexRow,indexCell)
+                        )
+                    })
+                }
+            </View>
+        )
+    }
+
+    renderCell(cell,indexRow,indexCell){
+        return(
+            <View style={{ flex: 1, alignSelf: 'stretch', borderWidth: "2px", borderColor: "black", color: "black !important", backgroundColor: "#e28743" }}>
+                {
+                    cell != "" ? 
+                        cell.includes("X") ? 
+                            <TouchableHighlight style={{justifyContent: 'center', minHeight: 50}} onPress={() => {this._alertIndex(indexRow,indexCell)}}>
+                                <Text style={{color: "black !important", textAlign: "center", paddingBottom: 10, paddingTop: 5}}>X</Text>
+                            </TouchableHighlight>
+                        : 
+                        <TouchableHighlight style={{justifyContent: 'center', minHeight: 50}} onPress={() => {this._alertIndex(indexRow,indexCell)}}>
+                            <Text style={{color: "black !important",textAlign: "center", paddingBottom: 10, paddingTop: 5}}>{cell}</Text>
+                        </TouchableHighlight>
+                    :
+                    <TouchableHighlight><Text></Text></TouchableHighlight>
+                }
+            </View>
+        )
+    }
+
+    _alertIndex(indexRow,indexCell) {
+        let newArr = [...this.state.tabella];
+        if(newArr[indexRow][indexCell].includes("X")){
+            newArr[indexRow][indexCell] = newArr[indexRow][indexCell].replace("X","")
+        }else{
+            newArr[indexRow][indexCell] = newArr[indexRow][indexCell] + "X";
+        }
+        this.setState({tabella: newArr})
+    }
+
+    render(){
+        return (
+            <View style={{ height: '20%', flexDirection: 'column', alignItems: 'center' }}>
+                {
+                    this.state.tabella.map((row,index) => {
+                        return (
+                            this.renderRow(row,index)
+                        )
+                    })
+                }
+            </View>
+        )      
+    }
+}
